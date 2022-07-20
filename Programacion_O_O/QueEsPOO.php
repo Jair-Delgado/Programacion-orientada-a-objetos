7c6cb4c64e293815472f8b14868e9d1b5ab53dfd
Jair-Delgado <102534932+Jair-Delgado@users.noreply.github.com>
2022-07-19 23:35:28 -0500

    Add files via upload

---
+++ Programacion_O_O/QueEsPOO.php
@@ -0,0 +1,14 @@
+<?php
+/***
+ * En POO todo es considerado como un objeto
+ * Un objeto es una clase que contiene atributos y métodos y puede heredar datos de otras y
+ * definir metodos especiales
+ * ej: Los atributos son las caracteristicas de una persona
+ * Los metodos son acciones
+ */ 
+
+ /**
+  * Métodos: Crear, Eliminar, Editar, Ver
+  * Atributos: ID, Titulo, Contenido, Fecha
+  */
+  ?>
---
+++ Programacion_O_O/objetosYMetodos.php
@@ -0,0 +1,29 @@
+<?php
+class Persona{
+
+    //Atributos se definen de una variable
+    public $nombre = 'Pedro';
+    
+
+    //Metodos se definen de una funcion
+    public function hablar($mensaje){
+        
+            echo $mensaje .;
+    }
+
+    
+}
+
+/***
+ * Las clases funcionan como un molde para un objeto
+ * Los objetos tienden a estar guardados en variables
+ */
+
+//new indica que estamos colocando una clase en un objeto
+$persona = new Persona();
+
+//-> se usa para llamar a un atributo o metodo
+echo $persona ->nombre; //Pedro
+
+// Debemos crear el objeto primero para luego llamar a sus atributos y metodos
+$persona ->hablar('esto es un mensaje');//esto es un mensaje
