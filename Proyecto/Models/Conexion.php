a72271a419ff90ee68dcc007f023bc53bebd8094
Jair-Delgado <jar.delgado@yavirac.edu.ec>
2022-07-21 15:15:22 -0500

    cap14

--- Proyecto/Models/Conexion.php
+++ Proyecto/Models/Conexion.php
@@ -2,6 +2,9 @@
 
 class Conexion
 {
+    public $con;
+    public $datos;
+
     public $datos = array
     (
         "host"=>"localhost", 
@@ -11,29 +14,13 @@ class Conexion
         "dbname"=>"proyecto"
     );
 
-    public $con;
 
     public function __construct()
     {
-        $this->con =  pg_connect($this->datos['host'], $this->datos['port'], $this->datos['user'], $this->datos['password'], $this->datos['dbname']);
-      
-        if( $this->con){
-
-            echo "Exito";
-           }else{
-    
-            echo "error". pg_last_error();
-    
-           }
-    }
-
-   /* public function dbconexion()
-    {
-        
-        //$this->con = pg_connect("host=localhost port=5432 user=postgres password=1234 dbname=proyecto");
+        $this->con = pg_connect("host=localhost port=5432 user=postgres password=1234 dbname=proyecto");
 
         //Verificar si se hizo la conexion
-        
+        /*
         if( $this->con){
 
             echo "Exito";
@@ -42,21 +29,33 @@ class Conexion
             echo "error". pg_last_error();
     
            }
-           
+      */
     }
 
-   /* public function consultaSimple($sql)
+  
+    
+   public function consultaSimple($sql)
     {
-        $this->con->query($sql);
+        $this->con ->query($sql);
     }
 
     public function consultaRetorno($sql)
     {
+        $datos = $this->$con->query($sql);
+        return $datos;
+    }
 
-    }*/
 
+    public function realizarConsulta()
+    {
+        $query = "SELECT * FROM estudiantes;";
+        $this->result = pg_query($query) or die('La consulta fallo: ' . pg_last_error());
+
+        echo  $this->result;
+        
+    }
 }
 
 $conexion = new Conexion();
-//$conexion->__construct();
+$conexion->realizarConsulta();
 //$conexion->dbconexion();
---
+++ Proyecto/Models/Estudiante.php
@@ -0,0 +1,52 @@
+<?php namespace Models;
+
+class Estudiante {
+
+    private $id;
+    private $nombre;
+    private $edad;
+    private $promedio;
+    private $imagen;
+    private $id_seccion;
+
+    public function __construct()
+    {
+         $this->con = new Conexion();
+    }
+
+    public function listar()
+    {
+        $sql ="SELECT t1.*, t2.nombre as nombre_seccion FROM estudiantes
+        INNER JOIN secciones t2 ON t1.id_seccion = tu.id";
+        $this->con->consultaRetorno($sql);
+        return $datos;
+    }
+    
+    public function add()
+    {
+        $sql = "INSERT INTO estudiantes VALUES(null,'{$this->nombre}', '{$this->edad}', '{$this->promedio}', '{$this->imagen}', '{$this->id_seccion}',)";
+
+        $this->con->consultaSimple($sql);
+    }
+
+    public function delete()
+    {
+        $sql = "DELETE FROM estudiantes WHERE id='{$this->id}'";
+        $this->con->consultaSimple($sql);
+    }
+
+    public function edir()
+    {
+        $sql = "UPDATE FROM estudiantes SET nombre='{$this->nombre}', edad='{$this->edad}', promedio='{$this->promedio}', id_seccion='{$this->id_seccion}'";
+        $this->consultaSimple($sql);
+    }
+
+    public function view()
+    {
+        $sql ="SELECT t1.*, t2.nombre as nombre_seccion FROM estudiantes t1 INNER JOIN
+         secciones t2 ON t1.id_seccion = t2.id WHERE id = '{$this->id}'";
+        $datos = $this->con->consultaRetorno("sql");
+        return $datos;
+    }
+    
+}
